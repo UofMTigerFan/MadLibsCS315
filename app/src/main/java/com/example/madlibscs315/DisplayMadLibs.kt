e4c5c61dbce1669860e233e2cfe9e904e86936bb
UofMTigerFan <42848825+UofMTigerFan@users.noreply.github.com>
2019-11-04 23:39:11 -0600

    Initial commit

---
+++ app/src/main/java/com/example/madlibscs315/DisplayMadLibs.kt
@@ -0,0 +1,50 @@
+package com.example.madlibscs315
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class DisplayMadLibs : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_display_mad_libs)
+
+        // Get the Intent that started this activity and extract the string
+        val message = intent.getStringExtra(EXTRA_MESSAGE)
+        val message2 = intent.getStringExtra(EXTRA_MESSAGE)
+        val message3 = intent.getStringExtra(EXTRA_MESSAGE)
+        val message4 = intent.getStringExtra(EXTRA_MESSAGE)
+        val message5 = intent.getStringExtra(EXTRA_MESSAGE)
+        val message6 = intent.getStringExtra(EXTRA_MESSAGE)
+        val message7 = intent.getStringExtra(EXTRA_MESSAGE)
+        val message8 = intent.getStringExtra(EXTRA_MESSAGE)
+
+        // Capture the layout's TextView and set the string as its text
+        val textView = findViewById<TextView>(R.id.textView).apply {
+            text = message
+        }
+
+        val textView2 = findViewById<TextView>(R.id.textView3).apply {
+            text = message2
+        }
+        val textView3= findViewById<TextView>(R.id.textView5).apply {
+            text = message3
+        }
+        val textView4 = findViewById<TextView>(R.id.textView7).apply {
+            text = message4
+        }
+        val textView5 = findViewById<TextView>(R.id.textView9).apply {
+            text = message5
+        }
+        val textView6 = findViewById<TextView>(R.id.textView11).apply {
+            text = message6
+        }
+        val textView7 = findViewById<TextView>(R.id.textView13).apply {
+            text = message7
+        }
+        val textView8 = findViewById<TextView>(R.id.textView15).apply {
+            text = message8
+        }
+    }
+}
---
+++ app/src/main/java/com/example/madlibscs315/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.madlibscs315
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+
+const val EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE"
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    /** Called when the user taps the Send button */
+    fun sendMessage(view: View) {
+        val editText = findViewById<EditText>(R.id.editText)
+        val editText2 = findViewById<EditText>(R.id.editText2)
+        val editText3 = findViewById<EditText>(R.id.editText3)
+        val editText4 = findViewById<EditText>(R.id.editText4)
+        val editText5 = findViewById<EditText>(R.id.editText5)
+        val editText6 = findViewById<EditText>(R.id.editText6)
+        val editText7 = findViewById<EditText>(R.id.editText7)
+        val editText8 = findViewById<EditText>(R.id.editText8)
+        val message1 = editText.text.toString()
+        val message2 = editText2.text.toString()
+        val message3 = editText3.text.toString()
+        val message4 = editText4.text.toString()
+        val message5 = editText5.text.toString()
+        val message6 = editText6.text.toString()
+        val message7 = editText7.text.toString()
+        val message8 = editText8.text.toString()
+        val intent = Intent(this, DisplayMadLibs::class.java).apply {
+            putExtra(EXTRA_MESSAGE, message1)
+        }
+        val intent2 = Intent(this, DisplayMadLibs::class.java).apply {
+            putExtra(EXTRA_MESSAGE, message2)
+        }
+        startActivity(intent)
+        startActivity(intent2)
+    }
+}
